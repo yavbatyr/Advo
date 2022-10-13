60cbb204b14622881b2fe823beb6dee83c95e336
yavbatyr <95848367+yavbatyr@users.noreply.github.com>
2022-10-13 22:00:08 +0300

    create section 'services'

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function(){
-
+    $('.services__tab-button').on('click',
+        function () {
+            $('.services__tab-button').removeClass('services__tab-button_active');
+            $(this).addClass('services__tab-button_active');
+        },
+    );
+    $('.services__tab-button').on('hover',
+        function() {
+            $('.services__tab-button').removeClass('services__tab-button_active');
+        }, 
+        function() {
+            $(this).addClass('services__tab-button_active');
+        }
+    );
 })
