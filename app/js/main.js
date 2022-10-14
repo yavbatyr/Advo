55d07f96eba1da60c245272a08b1ea0701dd5001
yavbatyr <95848367+yavbatyr@users.noreply.github.com>
2022-10-14 22:33:47 +0300

    updated accardeon in section 'services'

--- app/js/main.js
+++ app/js/main.js
@@ -5,12 +5,17 @@ $(function(){
             $(this).addClass('services__tab-button_active');
         },
     );
-    $('.services__tab-button').on('hover',
-        function() {
-            $('.services__tab-button').removeClass('services__tab-button_active');
-        }, 
-        function() {
-            $(this).addClass('services__tab-button_active');
-        }
-    );
+    $('.tab-button').on('click', function () {
+        var tabName = $(this).attr('data-tab');
+        var tab = $('.tab-content[data-tab="'+tabName+'"]');
+        $('.tab-content').removeClass('tab-content_active');
+        tab.addClass('tab-content_active');
+
+        console.log(tabName);
+
+    });
+    // $('.services__tab-button').on('click', function () {
+    //     $('.tab-content').css('display', 'flex');
+    //     $(this).css('display', 'none');
+    // });
 })
